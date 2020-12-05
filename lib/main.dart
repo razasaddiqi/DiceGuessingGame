2359df0bb86ffb458b741d3a4881123ce5e246dc
UMAIR <umair.nawaz1997@gmail.com>
2020-12-05 16:38:28 +0500

    Added Drawer ...

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,42 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Center(child: Text('Guessing Game', style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),),),
+              decoration: BoxDecoration(
+                color: Colors.black54,
+              ),
+            ),
+            ListTile(
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Contact Us'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       backgroundColor: Colors.grey.shade900,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
